38e555238b76eca30c0af488e1ddb59a9d405f80
Cookiecrafty <thomas.boinet75@gmail.com>
2025-05-05 17:01:23 +0200

    update AI

--- Source/ProtoCharon/ProtoCharon.Build.cs
+++ Source/ProtoCharon/ProtoCharon.Build.cs
@@ -8,7 +8,7 @@ public class ProtoCharon : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule","NavigationSystem","GameplayTasks","StateTreeModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
